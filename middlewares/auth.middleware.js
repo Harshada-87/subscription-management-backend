2f1479347ecc5c23d03a12b06b84e9ae15e6a830
Harshada-87 <rharshada542@gmail.com>
2026-04-23 17:25:33 +0530

    feat: update authorization middleware and secure user retrieval route

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,18 +1,17 @@
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "../config/env";
+import { JWT_SECRET } from "../config/env.js";
 import User from "../models/user.model.js";
 
-export const authorizeUser = async (req, res, next) => {
+const authorizeUser = async (req, res, next) => {
   try {
     let token;
 
     if (
-      req.headers.authorization ||
+      req.headers.authorization &&
       req.headers.authorization.startsWith("Bearer")
     ) {
       token = req.headers.authorization.split(" ")[1];
     }
-
 
     if (!token) return res.status(401).json({ message: "Unauthorized" });
     const decoded = jwt.verify(token, JWT_SECRET);
@@ -20,9 +19,11 @@ export const authorizeUser = async (req, res, next) => {
     const user = await User.findById(decoded.userId);
     if (!user) return res.status(401).json({ message: "Unauthorized" });
 
-    req.user =  user;
+    req.user = user;
     next();
   } catch (error) {
-      res.status(401).json({message:'Unauthorized', error: error.message});
+    res.status(401).json({ message: "Unauthorized", error: error.message });
   }
 };
+
+export default authorizeUser;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,11 @@
 import {Router} from 'express'
 import { getUser, getUsers } from '../controllers/user.controller.js';
+import authorizeUser from '../middlewares/auth.middleware.js';
 
 const userRouter = Router();
 
 userRouter.get('/', getUsers)
-userRouter.get('/:id', getUser)
+userRouter.get('/:id', authorizeUser, getUser)
 
 
 export default userRouter;
