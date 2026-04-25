87a29676ca9907664e50078d3721076386577600
Harshada-87 <rharshada542@gmail.com>
2026-04-25 12:12:47 +0530

    feat: enhance app structure with environment configuration and database connection

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
-import express from 'express';
-const app =  express();
+import express from "express";
+import { PORT } from "./config/env.js";
+import connectToDB from "./database/mongodb.js";
 
-app.get('/' , (req, res) =>{
-     res.send('Welcome to the Subscription Tracker API!');
+import authRouter from './routes/auth.routes.js';
+import userRouter from "./routes/user.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());  
+
+app.use('/api/v1/auth', authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use(errorMiddleware);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Subscription Tracker API!");
 });
 
-app.listen(3000, ()=>{
-    console.log('Subscription Tracker API is running on http://localhost:3000');
-})
+app.listen(PORT, async () => {
+  console.log(
+    `Subscription Tracker API is running on http://localhost:${PORT}`,
+  );
+
+  await connectToDB();
+});
+
+
 
 export default app;
